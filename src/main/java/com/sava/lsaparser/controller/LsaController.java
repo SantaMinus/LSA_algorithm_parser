82e033af4c4844cc05d2cf0016912833ac99da38
santaminus <santaminus@gmail.com>
2021-11-27 23:35:12 +0200

    Create first Thymeleaf View

--- src/main/java/com/sava/lsaparser/controller/LsaController.java
+++ src/main/java/com/sava/lsaparser/controller/LsaController.java
@@ -1,17 +1,16 @@
 package com.sava.lsaparser.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/lsa")
 public class LsaController {
-    @PostMapping
-    public void createLsa(@RequestBody String lsaString) {
-        log.info("LSA input: {}", lsaString);
+    @GetMapping
+    public String showLsaInputForm() {
+        return "lsaInput";
     }
 }
