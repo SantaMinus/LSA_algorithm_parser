8b9f500b11a657ef9f7a525bdef19d00f64135c0
santaminus <santaminus@gmail.com>
2021-11-29 23:29:20 +0200

    Modify GET; add POST method

--- src/main/java/com/sava/lsaparser/controller/LsaController.java
+++ src/main/java/com/sava/lsaparser/controller/LsaController.java
@@ -1,8 +1,12 @@
 package com.sava.lsaparser.controller;
 
+import com.sava.lsaparser.dto.Lsa;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -10,7 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/lsa")
 public class LsaController {
     @GetMapping
-    public String showLsaInputForm() {
+    public String showLsaInputForm(Model model) {
+        model.addAttribute("lsa", new Lsa());
         return "lsaInput";
+    }
+
+    @PostMapping
+    public String createLsa(@ModelAttribute Lsa lsa, Model model) {
+        model.addAttribute("lsa", lsa);
+        return "inputFormResult";
     }
 }
---
+++ src/main/java/com/sava/lsaparser/dto/Lsa.java
@@ -0,0 +1,10 @@
+package com.sava.lsaparser.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Lsa {
+    private String algorithmInput;
+}
