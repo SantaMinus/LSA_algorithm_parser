5cc922a10c9c565883601ee7f826eb3081cd6f8e
santaminus <santaminus@gmail.com>
2021-12-04 18:33:38 +0200

    Clean up a bit

--- src/main/java/com/sava/lsaparser/controller/LsaController.java
+++ src/main/java/com/sava/lsaparser/controller/LsaController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,7 +19,7 @@ public class LsaController {
     }
 
     @PostMapping
-    public String createLsa(@ModelAttribute Lsa lsa, Model model) {
+    public String createLsa(Lsa lsa, Model model) {
         model.addAttribute("lsa", lsa);
         return "inputFormResult";
     }
