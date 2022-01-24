ebd01558edba756f13471c689a87a5f64f4b77fc
santaminus <santaminus@gmail.com>
2022-01-24 20:00:00 +0200

    Use H2 in tests

--- src/test/groovy/com.sava.lsaparser.controller/LsaControllerIT.groovy
+++ src/test/groovy/com.sava.lsaparser.controller/LsaControllerIT.groovy
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class LsaControllerIT extends Specification {
     @Autowired
     private MockMvc mockMvc
