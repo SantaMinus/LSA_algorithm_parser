0a3ff0f0772e520242d331dcda44ce8c1384a630
santaminus <santaminus@gmail.com>
2022-04-03 22:11:13 +0300

    Test get users from DB

--- src/test/java/com/sava/lsaparser/controller/UserControllerIT.java
+++ src/test/java/com/sava/lsaparser/controller/UserControllerIT.java
@@ -1,5 +1,8 @@
 package com.sava.lsaparser.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,11 +16,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class UserControllerIT {
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
 
     @Test
-    void test() throws Exception {
+    void testGetUsers() throws Exception {
         mockMvc.perform(get("/user"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetUsersFromDb() throws Exception {
+        String res = mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        JSONArray array = mapper.readValue(res, JSONArray.class);
+        Assertions.assertEquals(3, array.size());
     }
 }
