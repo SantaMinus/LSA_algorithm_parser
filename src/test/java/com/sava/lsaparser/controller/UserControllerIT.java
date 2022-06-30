10a0316e388510116cafbb907c99eb7e10e97997
SantaMinus <santaminus@gmail.com>
2022-06-30 18:21:46 +0300

    Move tests to the controller set

--- src/test/java/com/sava/lsaparser/controller/UserControllerIT.java
+++ src/test/java/com/sava/lsaparser/controller/UserControllerIT.java
@@ -3,6 +3,8 @@ package com.sava.lsaparser.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.sava.lsaparser.dto.UserDto;
 import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,7 +15,10 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -36,12 +41,36 @@ class UserControllerIT {
 
     @Test
     @DatabaseSetup("/dataset/users.xml")
-//    @DatabaseTearDown("/dataset/cleanDb.xml")
+    @DatabaseTearDown("/dataset/cleanDb.xml")
     void testGetUsersFromDb() throws Exception {
         String res = mockMvc.perform(get("/user"))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
         JSONArray array = mapper.readValue(res, JSONArray.class);
         Assertions.assertEquals(3, array.size());
+    }
+
+    @Test
+    @DatabaseSetup("/dataset/cleanDb.xml")
+    void testCreateUser() throws Exception {
+        String username = "johnDoe";
+        String email = "johndoe@domain.com";
+        String pass = "12345";
+        UserDto newUser = new UserDto();
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+        newUser.setPassword(pass);
+
+        String result = mockMvc.perform(post("/user")
+                        .content(mapper.writeValueAsString(newUser))
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        UserDto created = mapper.readValue(result, UserDto.class);
+
+        assertNotNull(created);
+        assertEquals(username, created.getUsername());
+        assertEquals(email, created.getEmail());
+        assertEquals(pass, created.getPassword());
     }
 }
