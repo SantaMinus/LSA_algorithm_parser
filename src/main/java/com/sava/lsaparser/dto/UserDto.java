1762defce55bb9e00c1c53f82e78b70ad036139d
santaminus <santaminus@gmail.com>
2023-01-16 19:51:29 +0200

    Update UserDto

--- src/main/java/com/sava/lsaparser/dto/UserDto.java
+++ src/main/java/com/sava/lsaparser/dto/UserDto.java
@@ -2,9 +2,26 @@ package com.sava.lsaparser.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class UserDto {
-    private String username;
-    private String email;
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
     private String password;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+    private String matchingPassword;
+    private String username;
 }
