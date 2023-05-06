411dd038a0c131ad6989e763d1e93219cfdb2476
SantaMinus <santaminus@gmail.com>
2023-05-06 19:32:28 +0300

    Roll back last commit

--- src/main/java/com/sava/lsaparser/config/SecurityConfig.java
+++ src/main/java/com/sava/lsaparser/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.sava.lsaparser.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,8 +17,6 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserDetailsService userDetailsService;
-  @Value("${spring.security.encoder.secret}")
-  private String secret;
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,6 +38,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Bean
   public PasswordEncoder encoder() {
-    return new StandardPasswordEncoder(secret);
+    return new StandardPasswordEncoder("53cr3t");
   }
 }
