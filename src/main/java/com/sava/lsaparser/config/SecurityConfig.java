6801523ac3397da69495c7dbe7fb7d3c165ac9d1
SantaMinus <santaminus@gmail.com>
2023-05-07 18:22:23 +0300

    Use BCrypt password encoder

--- src/main/java/com/sava/lsaparser/config/SecurityConfig.java
+++ src/main/java/com/sava/lsaparser/config/SecurityConfig.java
@@ -8,8 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService)
-        .passwordEncoder(encoder());
+        .passwordEncoder(bCryptPasswordEncoder());
   }
 
   @Override
@@ -37,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
-  public PasswordEncoder encoder() {
-    return new StandardPasswordEncoder("53cr3t");
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
